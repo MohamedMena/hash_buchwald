012a53710eb48a3c4250bd51517c203cff834f9d
moha <mohamed.mena@gmail.com>
2018-10-21 22:31:23 -0300

    se agregaron primitivas iterador

--- hash.c
+++ hash.c
@@ -25,6 +25,19 @@ struct hash_iter{
 	 lista_iter_t* iter_actual;
  };
 
+size_t hash_iter_buscar_siguiente(lista_t** tabla, size_t pos_inicio, size_t capacidad){
+	size_t nueva_posicion = capacidad;
+	bool encontro_siguiente = false;
+	for (int i = pos_inicio; i < capacidad && !encontro_siguiente; ++i){
+		encontro_siguiente = (tabla[i] != NULL);
+		if(encontro_siguiente) nueva_posicion = i;
+	}
+	if(!encontro_siguiente && pos_inicio == 0) nueva_posicion = 0;
+
+	return nueva_posicion;
+}
+
+
 char *duplicar_clave(const char *clave){
 	size_t largo = strlen(clave) + 1;
 	char *dup_clave = malloc(sizeof(char)*largo);
@@ -111,4 +124,55 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 	hash->cantidad++;
 
 	return true;
+}
+
+hash_iter_t *hash_iter_crear(const hash_t *hash){
+	hash_iter_t* iterador = malloc(sizeof(hash_iter_t));
+	if(!iterador) return NULL;
+
+	iterador->hash = hash;
+	iterador->pos_actual = hash_iter_buscar_siguiente(hash->tabla, 0, hash->capacidad);
+	iterador->iter_actual = NULL;
+	if(iterador->hash->tabla[iterador->pos_actual] != NULL){
+		iterador->iter_actual = lista_iter_crear(iterador->hash->tabla[iterador->pos_actual]);
+	}
+	return iterador;
+}
+
+bool hash_iter_avanzar(hash_iter_t *iter){
+	bool avanzo = false;
+	if(iter->iter_actual != NULL){
+		avanzo = lista_iter_avanzar(iter->iter_actual);
+		if(lista_iter_al_final(iter->iter_actual)){
+			lista_iter_destruir(iter->iter_actual);
+			iter->pos_actual = hash_iter_buscar_siguiente(iter->hash->tabla, iter->pos_actual+1, iter->hash->capacidad);
+			if(iter->pos_actual == iter->hash->capacidad){
+				iter->iter_actual = NULL;
+				return false;
+			}
+			iter->iter_actual = lista_iter_crear(iter->hash->tabla[iter->pos_actual]);
+		}
+	}
+	return avanzo;
+}
+
+const char *hash_iter_ver_actual(const hash_iter_t *iter){
+	if(!iter->iter_actual) return NULL;
+	if(lista_iter_al_final(iter->iter_actual)) return NULL;
+
+	nodo_hash_t* nodo_actual = lista_iter_ver_actual(iter->iter_actual);
+	return nodo_actual->clave;
+}
+
+bool hash_iter_al_final(const hash_iter_t *iter){
+	if(!iter->iter_actual) return true;
+
+	if(lista_iter_al_final(iter->iter_actual) && iter->pos_actual == iter->hash->capacidad-1) return true;
+
+	return false;
+}
+
+void hash_iter_destruir(hash_iter_t* iter){
+	lista_iter_destruir(iter->iter_actual);
+	free(iter);
 }
